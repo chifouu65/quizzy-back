31641e640dcec4b78ba1952735172c571d147bd8
chenalkillian <113968865+chenalkillian@users.noreply.github.com>
2025-01-23 15:57:58 +0100

    issue 2

---
+++ src/firestore/firestore.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { FirestoreService } from './firestore.service';
+
+@Controller('/firestore')
+export class FirestoreController {
+
+
+
+}

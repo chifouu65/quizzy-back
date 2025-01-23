55e95ba7aae9fdad3f6958b534d7107e83833613
Noah <89213023+chifouu65@users.noreply.github.com>
2025-01-23 15:01:27 +0100

    Add Ping module with controller and service; enable CORS in main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { PingModule } from './ping/ping.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, PingModule],
   controllers: [AppController],
   providers: [AppService],
 })

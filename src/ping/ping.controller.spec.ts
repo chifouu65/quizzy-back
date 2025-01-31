49c3b605487f408d5e28ee349536f55a7da687d5
Noah <89213023+chifouu65@users.noreply.github.com>
2025-01-31 12:02:36 +0100

    test: ping controller

--- src/ping/ping.controller.spec.ts
+++ src/ping/ping.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { PingController } from './ping.controller';
 import { PingService } from './ping.service';
 
 describe('PingController', () => {
   let controller: PingController;
+  let service: PingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -12,9 +14,29 @@ describe('PingController', () => {
     }).compile();
 
     controller = module.get<PingController>(PingController);
+    service = module.get<PingService>(PingService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return success response', async () => {
+    const response = { status: 'OK', details: { database: 'OK' } };
+    jest.spyOn(service, 'pong').mockResolvedValue(response);
+
+    expect(await controller.ping()).toEqual(response);
+  });
+
+  it('should return error response', async () => {
+    const error = {
+      status: 'Partial',
+      details: {
+        database: 'KO',
+      },
+    };
+    jest.spyOn(service, 'pong').mockRejectedValue(error);
+
+    await expect(controller.ping()).rejects.toEqual(error);
   });
 });

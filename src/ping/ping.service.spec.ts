0c65cb2a750ffa94562367fc6cd9f7350820cc76
Noah <89213023+chifouu65@users.noreply.github.com>
2025-01-31 12:03:51 +0100

    test: ping service

--- src/ping/ping.service.spec.ts
+++ src/ping/ping.service.spec.ts
@@ -1,8 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PingService } from './ping.service';
+import * as admin from 'firebase-admin';
+
+jest.mock('firebase-admin', () => ({
+  firestore: jest.fn(() => ({
+    collection: jest.fn(() => ({
+      doc: jest.fn(() => ({
+        get: jest.fn(),
+      })),
+    })),
+  })),
+}));
 
 describe('PingService', () => {
   let service: PingService;
+  let getMock: jest.Mock;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -10,9 +22,38 @@ describe('PingService', () => {
     }).compile();
 
     service = module.get<PingService>(PingService);
+    getMock = jest.fn();
+    jest.spyOn(admin, 'firestore').mockImplementation(
+      () =>
+        ({
+          collection: () => ({
+            doc: () => ({
+              get: getMock,
+            }),
+          }),
+        }) as any,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return OK when database responds', async () => {
+    getMock.mockResolvedValueOnce({});
+    const result = await service.pong();
+    expect(result).toEqual({
+      status: 'OK',
+      details: { database: 'OK' },
+    });
+  });
+
+  it('should return KO when database fails', async () => {
+    getMock.mockRejectedValueOnce(new Error('Database error'));
+    const result = await service.pong();
+    expect(result).toEqual({
+      status: 'Partial',
+      details: { database: 'KO' },
+    });
   });
 });

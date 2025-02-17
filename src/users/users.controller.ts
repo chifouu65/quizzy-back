48d814bff8add7490b082167d3bbff8529e195e7
Noah <89213023+chifouu65@users.noreply.github.com>
2025-02-17 12:00:49 +0100

    feat: Implement authentication middleware and Firebase auth integration

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { cp } from 'fs';
 
-@Controller('users')
+@Controller('api/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('me')
   me() {
     return this.usersService.me();
+  }
+
+  @Post()
+  create(@Request() req) {
+    return this.usersService.create(req);
   }
 }
